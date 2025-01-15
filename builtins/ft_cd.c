42eeb22030a5babc6f1f349145c2c730a9662b70
eymarlk7 <rafaelkapalandanda@gmail.com>
2025-01-15 14:50:42 +0100

    ft_cd feito

--- builtins/ft_cd.c
+++ builtins/ft_cd.c
@@ -1,6 +1,15 @@
 #include "../minishell.h"
+#include <sys/types.h>
+#include <dirent.h>
 
 void    ft_cd(char **matrix)
 {
-    printf("test: %s\n", matrix[0]);
+    char    buf[255];
+
+    if (matrix[2] != NULL)
+        printf("%s: too many arguments\n", matrix[0]);
+    else if (chdir(matrix[1]) == 0)
+        printf("currently directory now: %s\n", getcwd(buf, sizeof(buf)));
+    else
+        printf("%s: %s: No such file or directory\n", matrix[0], matrix[1]);
 }
